92dc4ebc4056b86e8829612a2aafd62d41e3a31b
ivit <52450165+Subtracted@users.noreply.github.com>
2025-06-24 12:21:22 +0200

    Delete middleware directory

--- middleware/adminOnly.js
+++
@@ -1,9 +0,0 @@
-const adminOnly = (req, res, next) => {
-    if (req.user && req.user.role === 'admin') {
-        next();
-    } else {
-        res.status(403).json({ message: 'Accès réservé aux administrateurs' });
-    }
-};
-
-module.exports = adminOnly; 
