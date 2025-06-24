cc88615bf7031d13bb5e2c3c041af7057fa7d6b5
ivit <52450165+Subtracted@users.noreply.github.com>
2025-06-24 14:12:49 +0200

    Update user.js
    modif console debug

--- api/user.js
+++ api/user.js
@@ -16,6 +16,8 @@ export default async function handler(req, res) {
     return;
   }
 
+
+
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Méthode non autorisée' });
   }
