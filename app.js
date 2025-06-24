3319739d542676769fedd54358cb78fe1ed42c9e
ivit <52450165+Subtracted@users.noreply.github.com>
2025-06-24 12:23:04 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const reservationsRouter = require('./routes/reservations');
-const authRouter = require('./routes/auth');
-const trajetsRouter = require('./routes/trajets');
-
-app.use('/api/admin', require('./routes/admin'));
-app.use('/api', reservationsRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/trajets', trajetsRouter); 
